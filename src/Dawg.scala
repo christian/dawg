b1faea74069da881e5e382ebbc674905c1fc9430
Cristi Prodan <cprodan@ptc.com>
2016-03-09 10:31:22 +0100

    Function for suggesting, simplification of search. Abstract of the tree

--- src/Dawg.scala
+++ src/Dawg.scala
@@ -2,7 +2,9 @@ import java.io.FileOutputStream
 import java.nio.{Buffer, ByteBuffer}
 import java.nio.file.{Files, Path, Paths}
 
+import scala.StringBuilder
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.reflect.io.File
 
@@ -35,8 +37,8 @@ class Dawg {
   // The string represents the path to the Node.
   var minimizedNodes = new mutable.HashMap[String, Node]()
 
-  val END_OF_BRANCH = 1
-  val END_OF_WORD = 2
+  val END_OF_BRANCH = 1 // specify that branching has finished
+  val END_OF_WORD = 2 // specify that word has finished
 
   /**
     * @return Number of nodes in the DAWG
@@ -158,13 +160,15 @@ class Dawg {
     * @param extPos external Position (see the toArray function)
     * @param startNode start node id
     * @param endNode end node id
-    * @param letter which letter we have from start node to end node
-    * @param extraFlag weather if end of children, end of word or both
+    * @param letter which letter we have on the edge from start node to end node
+    * @param extraFlag END_OF_BRANCH = this node is the last child of it's parent's children,
+    *                  END_OF_WORD = end of word
+    *                  END_OF_WORD | END_OF_BRANCH = both last children and end of word
     */
   case class Edge(extPos: Int, startNode: Int, endNode: Int, letter: Char, extraFlag: Int)
 
   /**
-    * Return the
+    * Return the edges in the Dawg.
     */
   def edges(): List[Edge] = {
     var edgeList: List[Edge] = List()
@@ -310,43 +314,127 @@ class Dawg {
 // Should be in Swift
 case class BinaryDawg(filePath: String) {
 
-  val path: Path = Paths.get(filePath)
-  var data: Array[Byte] = Files.readAllBytes(path)
+  val END_OF_BRANCH = 1 // specify that branching has finished
+  val END_OF_WORD = 2 // specify that word has finished
 
-  def lookup(word: String): Boolean = { search(word, 0, 0) }
+  // Note that there might be an overhead memory wise with this approach.
+  // Although, these would be allocated on demand
+  case class BinNode(letter: Char, extraFlag: Int, ptr: Int) {
 
-  private def search(word: String, posCurrentLetter: Int, seek: Int): Boolean = {
-    if (posCurrentLetter >= word.length) { // all letters have been tested
-      true // word was found
-    } else {
-      // create an Int from first 4 bytes
-      val arrOfBytes = data.slice(seek * 4, seek * 4 + 4)
-      val theInt = ByteBuffer.wrap(arrOfBytes).getInt // Big Endian
-      val (ptr, extraFlag, letter) = fromInt(theInt)
+    def children(): List[BinNode] = {
+      var tmpChildren: ListBuffer[BinNode] = ListBuffer()
+      var shouldStop = false // extraFlag == END_OF_BRANCH
 
-      if (letter == word(posCurrentLetter)) {
-        search(word, posCurrentLetter + 1, ptr) // move on to next letter
-      } else {
-        var shouldStop = extraFlag != 0
-        var i = 1
-        while (!shouldStop) { // e.g has end-of-word been reached?
+      if (ptr == 0 && extraFlag != 0)
+        shouldStop = true // leaf nodes that have no children; their child points by default to root
 
-          val arrOfBytes = data.slice(seek * 4 + i * 4, seek * 4 + i * 4 + 4)
-          val theInt = ByteBuffer.wrap(arrOfBytes).getInt
-          val (ptr, extraFlag, letter) = fromInt(theInt)
+      var i = 0
 
-          if (letter == word(posCurrentLetter)) {
-            return search(word, posCurrentLetter + 1, ptr)
-          } else {
-            i += 1
-            shouldStop = extraFlag != 0
-          }
-        }
-        false
+      while (!shouldStop) {
+        val arrOfBytes = data.slice(ptr * 4 + i * 4, ptr * 4 + i * 4 + 4) // data.slice(i * 4, i * 4 + 4)
+        val theInt = ByteBuffer.wrap(arrOfBytes).getInt
+        val (p, e, l) = fromInt(theInt) // pointer, extraFlag, letter
+        val child = BinNode(l, e, p)
+        tmpChildren += child
+        shouldStop = e != 0 // either END_OF_BRANCH, or END_OF_WORD or both
+        i += 1
       }
+
+      tmpChildren.toList
     }
 
   }
+
+  val path: Path = Paths.get(filePath)
+  val data: Array[Byte] = Files.readAllBytes(path)
+  val root: BinNode = BinNode('-', 0, 0)
+
+  def findLastNodeInPrefix(prefix: String, node: BinNode): Option[BinNode] = {
+    if (prefix == "") {
+      Some(node)
+    } else {
+      var foundNode: Option[BinNode] = None
+      for (child <- node.children()) {
+        if (child.letter == prefix.head) {
+          foundNode = findLastNodeInPrefix(prefix.tail, child)
+        }
+      }
+      foundNode
+    }
+  }
+
+  var visited: ListBuffer[BinNode] = mutable.ListBuffer()
+  def getSufixes(node: BinNode, tmpSuffix: String, acc: List[String]): List[String] = {
+    visited += node
+    val children: List[BinNode] = node.children()
+    if (children.isEmpty) {
+      tmpSuffix.toString :: acc
+    } else {
+      children.filter(child => !visited.contains(child)).flatMap { child =>
+        getSufixes(child, tmpSuffix + child.letter, acc)
+      }
+    }
+  }
+
+  def suggest(prefix: String): List[String] = {
+    val node = findLastNodeInPrefix(prefix, root)
+    node match {
+      case Some(n) => getSufixes(n, "", List()).map { prefix + _}
+      case None => List()
+    }
+  }
+
+  def lookup(word: String): Boolean = { search(word, root) }
+
+  
+  // FIXME at the moment, it returns also true if the prefix exists in the tree
+  def search(word: String, node: BinNode): Boolean = {
+    if (word == "") { // word was "consumed"
+      true
+    } else {
+      node.children().foldLeft(false) { (acc, child) =>
+        if (child.letter == word.head)
+          acc || search(word.tail, child)
+        else
+          acc
+      }
+    }
+  }
+
+  // FIXME at the moment, it returns also true if the prefix exists in the tree
+  // Note: this is sort of a BFS
+//  private def search(word: String, posCurrentLetter: Int, seek: Int): Boolean = {
+//    if (posCurrentLetter >= word.length) { // all letters have been tested
+//      true // word was found
+//    } else {
+//      // create an Int from first 4 bytes
+//      val arrOfBytes = data.slice(seek * 4, seek * 4 + 4)
+//      val theInt = ByteBuffer.wrap(arrOfBytes).getInt // Big Endian
+//      val (ptr, extraFlag, letter) = fromInt(theInt)
+//
+//      if (letter == word(posCurrentLetter)) {
+//        search(word, posCurrentLetter + 1, ptr) // move on to next letter
+//      } else {
+//        var shouldStop = extraFlag != 0
+//        var i = 1
+//        while (!shouldStop) { // e.g has end-of-word been reached?
+//
+//          val arrOfBytes = data.slice(seek * 4 + i * 4, seek * 4 + i * 4 + 4)
+//          val theInt = ByteBuffer.wrap(arrOfBytes).getInt
+//          val (ptr, extraFlag, letter) = fromInt(theInt)
+//
+//          if (letter == word(posCurrentLetter)) {
+//            return search(word, posCurrentLetter + 1, ptr)
+//          } else {
+//            i += 1
+//            shouldStop = extraFlag != 0
+//          }
+//        }
+//        false // shouldStop is true at this point and the letter was not found on any branch
+//      }
+//    }
+//
+//  }
 
   def suggest() = {}
 
@@ -396,9 +484,12 @@ object DawgTest {
     val binDawg = BinaryDawg("data/words.dwg")
 
     println("Searching")
-    List("cats", "cale", "frig", "draw").foreach { word =>
+    List("cats", "cale", "frig", "draw", "craw", "brown").foreach { word =>
       println(s"$word ${binDawg.lookup(word)}")
     }
+
+    println("Suggesting")
+    println(binDawg.suggest("ca"))
 
   }
 
